03c22b5862487f6cb5b42c4d89fad246cef00073
aceawan <ladeveze.quentin@openmailbox.org>
2017-11-25 17:26:20 +0100

    occur check (suppression de fonctions inutiles)

--- unifie.pl
+++ unifie.pl
@@ -1,6 +1,4 @@
 % Occur check
-% occur_check(_,T) :- atomic(T). % Si T est atomique, X ne peut pas apparaître dans T
-% occur_check(_,T) :- var(T). % Si T est une variable, soi elle est différente de X et donc X n'apparait pas dans T, soit elle est identique à T et la règle d'occur check n'échoue pas
 occur_check(V,T) :- var(V),compound(T), is_var_in_term(V,T). % Si T n'est ni une variable, ni un atome, il est composé, on vérifie si X apparait dans le terme
 
 is_var_in_term(V,T) :- var(T), V == T.
