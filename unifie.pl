d9490b11f4e29fd842d2b44e248c028bcda012b1
François Lallemand <frlallemand@protonmail.com>
2017-11-26 11:51:42 +0100

    modifie check_nth_arg

--- unifie.pl
+++ unifie.pl
@@ -4,6 +4,9 @@ occur_check(V,T) :- var(V),compound(T), is_var_in_term(V,T). % Si T n'est ni une
 is_var_in_term(V,T) :- var(T), V == T.
 is_var_in_term(V,T) :- compound(T), functor(T,_,A), check_nth_arg(V,T,1,A).
 
-check_nth_arg(V, T, K, K) :- arg(K,T,X), is_var_in_term(V,X).
-check_nth_arg(V, T, K, A) :- K \= A, arg(K,T,X), is_var_in_term(V,X). 
-check_nth_arg(V, T, K, A) :- K \= A, plus(K,1,Y), check_nth_arg(V,T,Y,A).
+%check_nth_arg(V, T, K, K) :- arg(K,T,X), is_var_in_term(V,X).
+%check_nth_arg(V, T, K, A) :- K \= A, arg(K,T,X), is_var_in_term(V,X).
+%check_nth_arg(V, T, K, A) :- K \= A, plus(K,1,Y), check_nth_arg(V,T,Y,A).
+
+check_nth_arg(V, T, A) :- A > 0, arg(A,T,X), is_var_in_term(V,X).
+check_nth_arg(V, T, A) :- A \= 1, plus(A, -1, Y), check_nth_arg(V,T,Y).
