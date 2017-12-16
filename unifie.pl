05076a04fefde60740ae18227c988ad6ec68a166
aceawan <ladeveze.quentin@openmailbox.org>
2017-12-16 14:17:33 +0100

    enlève un prémisse inutile dans le prédicat regle de la règle rename

--- unifie.pl
+++ unifie.pl
@@ -26,17 +26,17 @@ echoln(_).
 % Si T est composé, il n'est pas une variable ou un atome, on vérifie si la variable V apparait dans T
 occur_check(V,T) :- var(V),compound(T), not(is_var_in_term(V,T)).
 
-% Si T et V sont identiques, la variable apparait dans le terme T
+% Condition d'arrêt de la récursion: T n'est plus qu'une simple variable. Si T et V sont identiques, la variable apparait dans le terme T
 is_var_in_term(V,T) :- var(T), V == T.
-% Si T est composé, on vérifie si la variable est un de ses arguments ou si elle apparait dans un de ses arguments
+% Si T est composé, on parcoure récursivement ses arguments pour vérifier la variable y apparait.
 is_var_in_term(V,T) :- compound(T), functor(T,_,A), check_nth_arg(V,T,A).
 
 % On itère sur les A arguments du terme T pour vérifier si la variable V apparait dans un argument de T.
 check_nth_arg(V, T, A) :- A > 0, arg(A,T,X), is_var_in_term(V,X).
 check_nth_arg(V, T, A) :- A \= 1, plus(A, -1, Y), check_nth_arg(V,T,Y).
 
-% Règles
-regle(X ?= Y, rename) :- var(X), var(Y), X = Y, !.
+% Le prédicat regle se satisfait lorsqu'une règle R peut s'appliquer à l'équation E.
+regle(X ?= Y, rename) :- var(X), var(Y), !.
 regle(X ?= Y, simplify) :- var(X), atomic(Y), !.
 regle(X ?= Y, expand) :- compound(Y), var(X), occur_check(X,Y), !.
 regle(X ?= Y, orient) :- not(var(X)), var(Y), !.
